15e1c884c595aec0bf79c31ee6ac4d259199393a
Konstantin2014 <87135556+Konstantin2014@users.noreply.github.com>
2022-09-07 10:53:32 +0300

    Использование Контекста

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
 import styled from "./HeaderCartButton.module.css";
 import CartIcon from "./CartIcon";
+import { useContext } from "react";
+import CartContext from "../../store/card-context";
 
 const HeaderCartButton = (props) => {
+  const cartContext = useContext(CartContext); // теперь HeaderCartButton будет переоцениваться react при ихменении CartContextю
+  // И контекст будет изменяться, когда мы будем его изменять в CartContextProviderю Таким образом мы устанавливаем соединение между контекстом и компонентами
+
+  const cartItemsNumber = cartContext.items.reduce((currentValue, item) => {
+    return currentValue + item.amount;
+  }, 0);
+
   return (
     <button className={styled.button} onClick={props.onClick}>
       <span className={styled.icon}>
         <CartIcon />
       </span>
       <span>Корзина</span>
-      <span className={styled.badge}>2</span>
+      <span className={styled.badge}>{cartItemsNumber}</span>
     </button>
   );
 };
